f74d4f01a08b1c4e853a4b223d5a49e50eb98c43
Omega0x013 <xanderbielby@gmail.com>
2021-01-04 10:37:37 +0000

    remove modernizr, add icon

--- main.js
+++ main.js
@@ -3,5 +3,7 @@
  */
 
 $(function() {
-	console.log("Hello World!");
+	if (!window.indexedDB) {
+		alert("Your browser does not support IndexedDB, you are not able to use Exam Maker. To fix this either change your browser settings or install another browser such as Google Chrome or Mozilla Firefox");
+	}
 });
