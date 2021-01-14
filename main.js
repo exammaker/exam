dea453ec62f2b8f916bba137ba88dd44367aa85f
Omega0x013 <xanderbielby@gmail.com>
2021-01-14 21:20:08 +0000

    add service worker

--- main.js
+++ main.js
@@ -3,7 +3,14 @@
  */
 
 $(function() {
-	if (!window.indexedDB) {
+	if (!window.indexedDB)
+	{
 		alert("Your browser does not support IndexedDB, you are not able to use Exam Maker. To fix this either change your browser settings or install another browser such as Google Chrome or Mozilla Firefox");
 	}
+	window.navigator.serviceWorker.register("/sw.js").then((registration) => {
+		console.log("ServiceWorker registered with scope: ", registration.scope);
+	}, (err) => {
+		console.log("ServiceWorker registration failed: ", err);
+	});
+	console.log("end")
 });
---
+++ sw.js
@@ -0,0 +1,30 @@
+// Service Worker for Exam Maker
+self.addEventListener('install', (event) => {
+	// This is where precaching occurs
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			if (response) {
+				return response;
+			}
+
+			return fetch(event.request).then(
+				function(response) {
+				if(!response || response.status !== 200 || response.type !== 'basic') {
+					return response;
+				}
+				var responseToCache = response.clone();
+
+				caches.open(CACHE_NAME)
+				.then(function(cache) {
+					cache.put(event.request, responseToCache);
+				});
+
+				return response;
+			});
+		})
+	);
+});
