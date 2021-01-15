c720bd1fa0ecbe2e95c67d65de183bef9cb68c90
Omega0x013 <xanderbielby@gmail.com>
2021-01-15 09:50:29 +0000

    add checks to sw register

--- main.js
+++ main.js
@@ -7,10 +7,15 @@ $(function() {
 	{
 		alert("Your browser does not support IndexedDB, you are not able to use Exam Maker. To fix this either change your browser settings or install another browser such as Google Chrome or Mozilla Firefox");
 	}
-	window.navigator.serviceWorker.register("/sw.js").then((registration) => {
-		console.log("ServiceWorker registered with scope: ", registration.scope);
-	}, (err) => {
-		console.log("ServiceWorker registration failed: ", err);
-	});
+	if(!window.isSecureContext) {
+		alert("This is not being served from a secure origin, exam maker will not work");
+	}
+	if('serviceWorker' in window.navigator) {
+		window.navigator.serviceWorker.register("/sw.js").then((registration) => {
+			console.log("ServiceWorker registered with scope: ", registration.scope);
+		}, (err) => {
+			console.log("ServiceWorker registration failed: ", err);
+		});
+	}
 	console.log("end")
 });
